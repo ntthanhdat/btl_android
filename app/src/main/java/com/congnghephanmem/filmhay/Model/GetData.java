5a15a591e15a9bbe42b0ebe15395123efd077310
Nguyễn Thế Thành Đạt <74961383+ntthanhdat@users.noreply.github.com>
2023-04-04 22:18:24 +0700

    init project

---
+++ app/src/main/java/com/congnghephanmem/filmhay/Model/GetData.java
@@ -0,0 +1,11 @@
+package com.congnghephanmem.filmhay.Model;
+
+public class GetData {
+    public static String email;
+    public static String phone;
+    public static String ten;
+    public static String avatar;
+
+    public GetData() {
+    }
+}
